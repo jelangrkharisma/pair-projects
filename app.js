6ed5b9aad8ab750f78f3a67adc60acd9020f7197
Gabriel <biel.jaeger03@gmail.com>
2019-12-18 16:15:27 +0700

    fix dynamic routes

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.render('home.ejs')
+    res.render('home.ejs', {wrong: false})
 })
 
 // PLAYERS ROUTE
--- routes/playerRoute.js
+++ routes/playerRoute.js
@@ -11,24 +11,12 @@ router.get('/', (req, res) => {
     Player.findAll({
         include: [Club]
     })
-        .then(rows => {
-            res.render('playerList.ejs', { players: rows })
-        })
-        .catch(err => {
-            res.send(err)
-        })
-})
-
-router.get('/:id', (req, res) => {
-    Player.findByPk(req.params.id, {
-        include: [Club]
+    .then(rows => {
+        res.render('playerList.ejs', { players: rows })
     })
-        .then(row => {
-            res.render('playerProfile.ejs', { player: row.get() })
-        })
-        .catch(err => {
-            res.send(err)
-        })
+    .catch(err => {
+        res.send(err)
+    })
 })
 
 router.get('/register', (req, res) => {
@@ -43,9 +31,39 @@ router.post('/register', (req, res) => {
         ClubId: null,
         username: req.body.username
     })
+    .then(row => {
+        console.log(row)
+        res.redirect(`player/${row.id}`)
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
+router.post('/login', (req, res) => {
+    Player.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(row => {
+        if (req.body.password === row.password) {
+            res.redirect(`player/${row.id}`)
+        } else {
+            res.render('home.ejs', {wrong: true})
+        }
+    })
+    .catch(err => {
+        res.send(err)
+    })
+})
+
+router.get('/:id', (req, res) => {
+    Player.findByPk(req.params.id, {
+        include: [Club]
+    })
         .then(row => {
-            console.log(row)
-            res.redirect(`/${row.id}`)
+            res.render('playerProfile.ejs', { player: row.get() })
         })
         .catch(err => {
             res.send(err)
