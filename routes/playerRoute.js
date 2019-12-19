dceab70becaf976cc6335655f8d994b6e720bc3f
Jelang R. Kharisma <53439995+jelangrkharisma@users.noreply.github.com>
2019-12-19 16:43:27 +0700

    finish openclubs

--- routes/playerRoute.js
+++ routes/playerRoute.js
@@ -12,20 +12,20 @@ router.get('/', (req, res) => {
     Player.findAll({
         include: [Club]
     })
-    .then(rows => {
-        players = rows
-        return MatchDetail.findAll()
-    })
-    .then(rows => {
-        const goals = [];
-        const fouls = [];
-        players.forEach(player => {
-            goals.push(p)
+        .then(rows => {
+            players = rows
+            return MatchDetail.findAll()
         })
-    })
-    .catch(err => {
-        res.send(err)
-    })
+        .then(rows => {
+            const goals = [];
+            const fouls = [];
+            players.forEach(player => {
+                goals.push(p)
+            })
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
 router.get('/register', (req, res) => {
@@ -40,13 +40,13 @@ router.post('/register', (req, res) => {
         ClubId: null,
         username: req.body.username
     })
-    .then(row => {
-        console.log(row)
-        res.redirect(`player/${row.id}`)
-    })
-    .catch(err => {
-        res.send(err)
-    })
+        .then(row => {
+            console.log(row)
+            res.redirect(`player/${row.id}`)
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
 router.post('/login', (req, res) => {
@@ -55,23 +55,25 @@ router.post('/login', (req, res) => {
             username: req.body.username
         }
     })
-    .then(row => {
-        if (req.body.password === row.password) {
-            res.redirect(`player/${row.id}`)
-        } else {
-            res.render('home.ejs', {wrong: true})
-        }
-    })
-    .catch(err => {
-        res.send(err)
-    })
+        .then(row => {
+            if (req.body.password === row.password) {
+                res.redirect(`player/${row.id}`)
+            } else {
+                res.render('home.ejs', { wrong: true })
+            }
+        })
+        .catch(err => {
+            res.send(err)
+        })
 })
 
 router.get('/player/:id', (req, res) => {
+
     Player.findByPk(req.params.id, {
         include: [Club]
     })
         .then(row => {
+            console.log(row);
             res.render('playerProfile.ejs', { player: row.get() })
         })
         .catch(err => {
